d7bb1816b410228f22226a9a48dac4b615c1569c
DIonGrimm <31693758+DionGrimm@users.noreply.github.com>
2020-02-16 18:01:17 +0100

    speeeed

--- miner.py
+++ miner.py
@@ -5,7 +5,7 @@ import json
 import sys
 import secrets
 
-sys.setrecursionlimit(150000)
+sys.setrecursionlimit(999999)
 
 def encrypt_string(hashString):
     sha_signature = \
@@ -24,7 +24,7 @@ stringFromPreviousBlock = f'{blockchain["hash"]}{blockchain["data"][0]["from"]}{
 hashedBlock = encrypt_string(hash(stringFromPreviousBlock))
 # Try to create a valid hash with different nonce values
 def findValidNonce():
-    for i in range(4000):
+    for i in range(999999):
         nonce = i + secrets.randbelow(99999)
         stringToHash = f'{hashedBlock}{transactions[0]["from"]}{transactions[0]["to"]}{transactions[0]["amount"]}{transactions[0]["timestamp"]}{timestamp}{nonce}'
         hashToCheck = encrypt_string(hash(stringToHash))
