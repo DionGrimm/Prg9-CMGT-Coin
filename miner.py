fa45ea6c3874f65dcefe431238b0fb46bac441ec
DIonGrimm <31693758+DionGrimm@users.noreply.github.com>
2020-02-16 16:34:12 +0100

    Update miner.py

--- miner.py
+++ miner.py
@@ -26,8 +26,8 @@ stringFromPreviousBlock = f'{blockchain["hash"]}{blockchain["data"][0]["from"]}{
 hashedBlock = encrypt_string(hash(stringFromPreviousBlock))
 # Try to create a valid hash with different nonce values
 def findValidNonce():
-    for i in range(5000):
-        nonce = i + 1000 + 3 * i
+    for i in range(4000):
+        nonce = i + 1000
         stringToHash = f'{hashedBlock}{transactions[0]["from"]}{transactions[0]["to"]}{transactions[0]["amount"]}{transactions[0]["timestamp"]}{timestamp}{nonce}'
         hashToCheck = encrypt_string(hash(stringToHash))
         print(nonce, hashToCheck)
