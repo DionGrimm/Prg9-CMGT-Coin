d6187d5f535ea20550cd7f8d79809c4efdde789e
DIonGrimm <31693758+DionGrimm@users.noreply.github.com>
2020-02-16 16:10:07 +0100

    Update miner.py

--- miner.py
+++ miner.py
@@ -15,43 +15,32 @@ def encrypt_string(hashString):
     return sha_signature
 
 url = 'https://programmeren9.cmgt.hr.nl:8000/api/blockchain/next'
+jsonData = json.dumps(requests.get(url).json())
+blockchain = json.loads(jsonData)['blockchain']
+transactions = json.loads(jsonData)['transactions']
+timestamp = json.loads(jsonData)['timestamp']
 
-def mine():
-    jsonData = json.dumps(requests.get(url).json())
-    blockchain = json.loads(jsonData)['blockchain']
-    transactions = json.loads(jsonData)['transactions']
-    timestamp = json.loads(jsonData)['timestamp']
+# Hash string from previous block
+stringFromPreviousBlock = f'{blockchain["hash"]}{blockchain["data"][0]["from"]}{blockchain["data"][0]["to"]}{blockchain["data"][0]["amount"]}{blockchain["data"][0]["timestamp"]}{blockchain["data"][0]["timestamp"]}{blockchain["nonce"]}'
 
-    # Hash string from previous block
-    stringFromPreviousBlock = f'{blockchain["hash"]}{blockchain["data"][0]["from"]}{blockchain["data"][0]["to"]}{blockchain["data"][0]["amount"]}{blockchain["data"][0]["timestamp"]}{blockchain["data"][0]["timestamp"]}{blockchain["nonce"]}'
-
-    hashedBlock = encrypt_string(hash(stringFromPreviousBlock))
-    time.sleep(1)
-    # Try to create a valid hash with different nonce values
-    def findValidNonce(count = 0):
-        count += 1
-        if count > 500: return mine()
-        nonce = secrets.randbelow(999999)
+hashedBlock = encrypt_string(hash(stringFromPreviousBlock))
+# Try to create a valid hash with different nonce values
+def findValidNonce():
+    for i in range(5000):
+        nonce = i + 1000 + 3 * i
         stringToHash = f'{hashedBlock}{transactions[0]["from"]}{transactions[0]["to"]}{transactions[0]["amount"]}{transactions[0]["timestamp"]}{timestamp}{nonce}'
         hashToCheck = encrypt_string(hash(stringToHash))
-        print(hashToCheck)
-        if hashToCheck.startswith('0000'): return nonce
-        return findValidNonce(count)
+        print(nonce, hashToCheck)
+        if hashToCheck.startswith('0000'): return str(nonce)
 
-    validNonce = findValidNonce()
-    print(validNonce)
-    # Post the valid nonce
-    urlForPost = 'https://programmeren9.cmgt.hr.nl:8000/api/blockchain'
-    data = {
-        'nonce': validNonce,
-        'user': 'Dion 0906233'
-    }
+validNonce = findValidNonce()
+print(validNonce)
+# Post the valid nonce
+url = 'https://programmeren9.cmgt.hr.nl:8000/api/blockchain'
+data = {
+    'nonce': validNonce,
+    'user': 'Dion 0906233'
+}
 
-    requests.post(urlForPost, json.dumps(data))
-
-mine()
-# schedule.every(1).minutes.do(mine)
-
-# while 1:
-#     schedule.run_pending()
-#     time.sleep(1)
+post = requests.post(url, data = data)
+print(post)
